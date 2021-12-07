0cd07e75af1ff7fd07111df0b1d73e8a34b7f6c4
Santiago Pérez <santiago.perez.mt@correo.usa.edu.co>
2021-12-07 16:26:02 -0500

    Agregadas buenas prácticas

--- backend-spring/src/main/java/g18/reto2/controlador/ControladorUser.java
+++ backend-spring/src/main/java/g18/reto2/controlador/ControladorUser.java
@@ -17,42 +17,72 @@ import org.springframework.web.bind.annotation.RestController;
 
 import g18.reto2.modelo.ModeloUser;
 import g18.reto2.servicios.ServiciosUser;
-
+/**
+ * 
+ * @author 
+ */
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
 public class ControladorUser {
+    /**
+     * 
+     */
     @Autowired
     private ServiciosUser UserService;
-
+    /**
+     * 
+     * @return 
+     */
     @GetMapping("/all")
     public List<ModeloUser> getAll() {
         return UserService.getAll();
     }
-    
+    /**
+     * 
+     * @param user
+     * @return 
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public ModeloUser create(@RequestBody ModeloUser user) {
         return UserService.create(user);
     }
-
+    /**
+     * 
+     * @param user
+     * @return 
+     */
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
     public ModeloUser update(@RequestBody ModeloUser user) {
         return UserService.update(user);
     }
-
+    /**
+     * 
+     * @param id
+     * @return 
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public boolean delete (@PathVariable("id") String id) {
         return UserService.delete(id);
     }
-    
+    /**
+     * 
+     * @param email
+     * @param password
+     * @return 
+     */
     @GetMapping("/{email}/{password}")
     public ModeloUser authenticateUser(@PathVariable("email") String email, @PathVariable("password") String password) {
         return UserService.authenticateUser(email, password);
     }
-
+    /**
+     * 
+     * @param email
+     * @return 
+     */
     @GetMapping("/emailexist/{email}")
     public boolean emailExists(@PathVariable("email") String email) {
         return UserService.emailExists(email);
